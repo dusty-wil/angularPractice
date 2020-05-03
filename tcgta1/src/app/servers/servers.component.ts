5e8d0f30e4310dc8f4449f378c509034b2551b51
Dusty Williams <dusty.wil@gmail.com>
2020-05-03 14:39:55 -0400

    event binding - dmw

--- tcgta1/src/app/servers/servers.component.ts
+++ tcgta1/src/app/servers/servers.component.ts
@@ -18,11 +18,18 @@ import { Component, OnInit } from '@angular/core';
   // ]
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
+  allowNewServer = true;
+  serverCreationStatus = 'Server not created';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+
 }
+
+
