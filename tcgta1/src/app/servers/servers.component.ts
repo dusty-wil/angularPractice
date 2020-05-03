0d9a5a9ec99fa9c521e06597c1eb67e318613a8c
Dusty Williams <dusty.wil@gmail.com>
2020-05-02 20:56:45 -0400

    component generation practice - dmw

--- tcgta1/src/app/servers/servers.component.ts
+++ tcgta1/src/app/servers/servers.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector can be any CSS selector IE
+  // <div app-servers></div>
+  // selector: '[app-servers]'
+  // selecting by class works, id does not
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  // template: '', components have to have either templateUrl or template
+  // template: ``, components have to have either templateUrl or template
   // use back-tics ` ` to do multi-line HTML
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  // alternative
+  // styles: [
+  //   `h3 { 
+  //     color: dodgerBlue;
+  //   }`
+  // ]
 })
 export class ServersComponent implements OnInit {
 
