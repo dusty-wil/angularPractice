7db208deb1d0fc51f9b17be616bba56c285e2cc1
= <=>
2020-05-13 23:05:10 -0400

    added new updates for recipe and ingredient models - dmw

--- recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      "strange bun", 
+      "this is a test", 
+      "https://c.pxhere.com/photos/0f/c5/mushroom_cuttlefish_fungus_fungal_species_clathrus_archeri_anthurus_archeri_gitterling_clathrus_red-829701.jpg!d"
+    ),
+    new Recipe(
+      "strange bun", 
+      "this is a test", 
+      "https://c.pxhere.com/photos/0f/c5/mushroom_cuttlefish_fungus_fungal_species_clathrus_archeri_anthurus_archeri_gitterling_clathrus_red-829701.jpg!d"
+    )
+  ];
 
   constructor() { }
 
