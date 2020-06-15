70810f02b40feedd086d00da5fc5c57e47174460
Dusty Williams <dusty.wil@gmail.com>
2020-06-14 22:55:31 -0400

    property and event binding - dmw

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,23 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [{
+    type: "server", 
+    name: "string", 
+    content: "string"
+  }];
 
-  onAddServer() {
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    console.log(serverData);
+    
     this.serverElements.push({
       type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: serverData.serverName,
+      content: serverData.serverContent
     });
   }
 
-  onAddBlueprint() {
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
     this.serverElements.push({
       type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
     });
   }
 }
